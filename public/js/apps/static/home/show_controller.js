d4fc3e680dc6af423e7005b167a63bc2ce1eac35
bobby <xaevir@gmail.com>
2016-05-19 00:05:26 -0400

    listino updates before nra

--- public/js/apps/static/home/show_controller.js
+++ public/js/apps/static/home/show_controller.js
@@ -1,4 +1,4 @@
-define([ 
+define([
   'appMarionette',
   'apps/static/home/show_view',
 //  'views/site/stopClick',
@@ -12,6 +12,10 @@ define([
         document.title = 'Rosito Bisani'
 
         showHome.on('navigate', function(target){
+          // adding for listino to go to manuals page
+          if (!target)
+            App.trigger('manuals:list', target);
+          else
           App.trigger('products:list', target);
         });
       })
